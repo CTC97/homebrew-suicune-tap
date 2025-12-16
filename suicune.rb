631b28a31dbae1e93660fcc5493bef34ae9c85e3
CTC97 <ctcurnin@gmail.com>
2025-12-15 22:41:46 -0500

    0d

--- suicune.rb
+++ suicune.rb
@@ -1,9 +1,9 @@
 class Suicune < Formula
   desc "2D game engine built on top of raylib."
   homepage "https://github.com/CTC97/suicune"
-  url "https://github.com/CTC97/suicune/archive/refs/tags/0c.tar.gz"
-  version "0c"
-  sha256 "fb9c65262d9834ab00b3ac27a37889d56d6d42915726b6b6dabe717e46894ac0"
+  url "https://github.com/CTC97/suicune/archive/refs/tags/0d.tar.gz"
+  version "0d"
+  sha256 "d5eb3d30d466e10bf44f5138d381bda3697d80105c8faa41def40820c13dcbe4"
   license "GPL-3.0-or-later"
 
   depends_on "raylib"
